8df188f6047df4e9d38890ccd23bc304ee635ad7
rf.cabral@hotmail.com <rf.cabral@hotmail.com>
2023-05-24 23:47:55 +0000

    Ola mundo do primeiro dia

---
+++ Ola Mundo.sql
@@ -0,0 +1,11 @@
+-- Databricks notebook source
+SELECT 'Ola Mundo'
+
+-- COMMAND ----------
+
+SELECT * -- selecione todas as colunas
+FROM silver.pizza_query.pedido -- da tabela pizza_query.pedido
+
+-- COMMAND ----------
+
+
